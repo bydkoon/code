68e21afb0e98c537bc822b40253a2314fa828163
bydkoon <pansew@gmail.com>
2022-03-30 00:49:19 +0900

    greedy 추가

--- algorithm/프로그래머스/탐욕법(Greedy)/조이스틱/solution.py
+++ algorithm/프로그래머스/탐욕법(Greedy)/조이스틱/solution.py
@@ -5,11 +5,34 @@ def solution(name: str):
     A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,W,X,Y,Z
     """
 
-    answer = 0
-    return answer
+    lst = [min(ord(n) - ord("A"), ord("Z")-ord(n)+1) for n in name]
+    # [9, 4, 9, 12, 4, 13]
+    print(sum(lst))
+    cnt = 0
+    loc = 0
+    print(lst)
+    while True:
+        cnt += lst[loc]
+        lst[loc] = 0
+        if sum(lst) == 0:
+            break
+        left = 1
+        right = 1
+        while lst[loc + right] == 0:
+            right += 1
+
+        while lst[loc - left] == 0:
+            left += 1
+        if left >= right:
+            loc += right
+            cnt += right
+        else:
+            loc -= left
+            cnt += left
+
+    return cnt
 
 
-
-if __name__ =="__main__":
-    name = ""
+if __name__ == "__main__":
+    name = "JEROEN"
     solution(name)
