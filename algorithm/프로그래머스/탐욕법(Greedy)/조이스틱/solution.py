52023b630dee64109831036efe597a8583801114
bydkoon <pansew@gmail.com>
2022-03-29 23:29:21 +0900

    greedy 추가

---
+++ algorithm/프로그래머스/탐욕법(Greedy)/조이스틱/solution.py
@@ -0,0 +1,15 @@
+
+def solution(name: str):
+
+    """
+    A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,W,X,Y,Z
+    """
+
+    answer = 0
+    return answer
+
+
+
+if __name__ =="__main__":
+    name = ""
+    solution(name)
