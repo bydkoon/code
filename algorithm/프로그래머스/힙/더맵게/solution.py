c7f3925d92185b5a16b7f48d04d5ae592c68dc14
bydkoon <pansew@gmail.com>
2022-04-09 11:45:51 +0900

    42626 힙 수정

--- algorithm/프로그래머스/힙/더맵게/solution.py
+++ algorithm/프로그래머스/힙/더맵게/solution.py
@@ -25,20 +25,23 @@ scoville	K	return
 """
 
 
-def solution(scoville, K):
-    scoville.sort()
-    count = 0
-    while True:
-        if scoville[0] >= K:
-            break
-        else:
-            d = scoville.pop(0) + (scoville.pop(0) * 2)
-            scoville.insert(0, d)
-            count += 1
-
-    return count
+import heapq
 
 
+def solution(scoville, k):
+    heap = []
+    for num in scoville:
+        heapq.heappush(heap, num)
+
+    mix_cnt = 0
+    while heap[0] < k:
+        try:
+            heapq.heappush(heap, heapq.heappop(heap) + (heapq.heappop(heap) * 2))
+        except IndexError:
+            return -1
+        mix_cnt += 1
+
+    return mix_cnt
 
 
 if __name__ == "__main__":
