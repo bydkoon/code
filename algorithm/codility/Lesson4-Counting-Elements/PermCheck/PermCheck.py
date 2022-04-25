7e0d51dd2749ceda12fed3decac4fd6191e6e1e8
bydkoon <pansew@gmail.com>
2022-04-25 23:31:57 +0900

    permcheck update

--- algorithm/codility/Lesson4-Counting-Elements/PermCheck/PermCheck.py
+++ algorithm/codility/Lesson4-Counting-Elements/PermCheck/PermCheck.py
@@ -1,6 +1,19 @@
 
 def solution(A):
-    return 
+    a = list(set(A))
+    if len(a) != len(A):
+        return 0
+    a.sort()
+    count = 1
+    for i in a:
+        if count == i:
+            result =1
+            count +=1
+        else:
+            result = 0
+            break
+    return result 
+
 
 if __name__ == "__main__":
     A = dict()
@@ -9,6 +22,6 @@ if __name__ == "__main__":
     A[2] = 3
     A[3] = 2
     
-    solution(A)
+    print(solution(A))
 
 
