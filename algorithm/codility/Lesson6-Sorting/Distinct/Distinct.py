158750595b386b7b2a007898a673839f0f3c55a0
Dohoon Kim <pansew@gmail.com>
2022-04-28 22:28:43 +0900

    lesson6 sorting directory add
    distinct solution

---
+++ algorithm/codility/Lesson6-Sorting/Distinct/Distinct.py
@@ -0,0 +1,28 @@
+"""
+
+"""
+def solution(A):
+    A.sort()
+    distict = 0
+    # [1,1,1,2,2,3]
+    N = len(A)
+
+    if N == 0: 
+        return 0
+
+    count = 1
+    for i in range(1, N):
+        if A[i-1] != A[i]:
+            count += 1
+    return count
+
+        
+            
+    return A
+
+if __name__ == "__main__":
+
+    A = [2,1,1,2,3,1]
+    print(solution(A))
+
+
