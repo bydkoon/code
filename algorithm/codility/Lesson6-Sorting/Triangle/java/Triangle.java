b1807eabe254e7755bbf8a73af9b419125a49211
Dohoon Kim <pansew@gmail.com>
2022-05-02 23:35:01 +0900

    traiangle solution 추가

--- algorithm/codility/Lesson6-Sorting/Triangle/java/Triangle.java
+++ algorithm/codility/Lesson6-Sorting/Triangle/java/Triangle.java
@@ -1,7 +1,23 @@
+import java.util.*;
 public class Triangle {
-    public void solution(int[] A){
+    public int solution(int[] A){
+        
+        Arrays.sort(A);
+        System.out.println(Arrays.toString(A));
+        for (int i = 0; i < A.length-2; i++) {
+            System.out.println(i);
+            if(A[i] > A[i+2] - A[i+1]){
+                return 1;
+            }
+            }
+
+        return 0;
     }
     
     public static void main(String[] args) {
+        int[] A = {-3, 1, 2, -2, 5, 6};
+        Triangle triangle = new Triangle();
+        int result = triangle.solution(A);
+        System.out.println(result);
     }
 }
