06940340351a5e6a1f955573bcf91dc804de6c13
Dohoon Kim <pansew@gmail.com>
2022-05-04 22:32:54 +0900

    greedy 탐욕법 문제 풀이

--- algorithm/codility/Lesson16-Greedy-algorithms/MaxNonoverlappingSegments/java/MaxNonoverlappingSegments.java
+++ algorithm/codility/Lesson16-Greedy-algorithms/MaxNonoverlappingSegments/java/MaxNonoverlappingSegments.java
@@ -1,7 +1,35 @@
-public class template {
-    public void solution(int[] A){
+
+class MaxNonoverlappingSegments {
+    public int solution(int[] A, int[] B){
+
+        int end = B[0];
+        int segment = 1;
+        if (A.length <= 1){
+            return A.length;
+        }
+        
+        for (int i = 1; i < A.length; i++) {
+            if(A[i] > end){
+                segment++;
+                end = B[i];
+
+
+                
+            }
+            
+        }
+
+
+        return segment;
     }
     
     public static void main(String[] args) {
+
+        int[] A = {1,3,7,9,9};
+        int[] B = {5,6,8,9,10};
+
+        MaxNonoverlappingSegments maxNonoverlappingSegments = new MaxNonoverlappingSegments();
+        maxNonoverlappingSegments.solution(A,B);
+
     }
 }
