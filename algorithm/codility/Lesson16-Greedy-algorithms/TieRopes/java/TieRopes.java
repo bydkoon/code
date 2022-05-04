575133397efd7c27a892877a8e84a45277c62b8f
Dohoon Kim <pansew@gmail.com>
2022-05-04 23:37:31 +0900

    greedy tieRopes solution 추가

--- algorithm/codility/Lesson16-Greedy-algorithms/TieRopes/java/TieRopes.java
+++ algorithm/codility/Lesson16-Greedy-algorithms/TieRopes/java/TieRopes.java
@@ -1,7 +1,27 @@
-public class template {
-    public void solution(int[] A){
+
+public class TieRopes {
+    public int solution(int K, int[] A){
+        
+        int count = 0;
+        int sumA = 0;
+        for (int i = 0; i < A.length; i++) {
+            sumA += A[i];
+            if(sumA >= K){
+                count++;
+                sumA = 0;
+            }
+
+
+        }
+        return count;
+
     }
     
     public static void main(String[] args) {
+        int K = 4;
+        int[] A = {1,2,3,4,1,1,3};
+        TieRopes tieRopes = new TieRopes();
+        int count = tieRopes.solution(K, A);
+        System.out.println(count);
     }
 }
