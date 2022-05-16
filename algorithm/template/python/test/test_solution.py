e7c96b7a61c5b6d7db8b7cf0e0248900cecf83e2
Dohoon Kim <pansew@gmail.com>
2022-05-16 19:53:07 +0900

    template python update

---
+++ algorithm/template/python/test/test_solution.py
@@ -0,0 +1,18 @@
+import unittest
+import sys
+from os import path
+print(path.dirname( path.dirname( path.abspath(__file__))))
+sys.path.append(path.dirname( path.dirname( path.abspath(__file__))))
+
+class TestSolution(unittest.TestCase):
+    
+    def test_solution(self):
+        H = ""
+        answer = ""
+        self.assertEqual(solution(H) , answer)
+
+
+
+# unittest를 실행
+if __name__ == '__main__':  
+    unittest.main()
