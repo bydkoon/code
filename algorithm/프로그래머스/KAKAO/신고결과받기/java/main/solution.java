9a50332191f4d1c15528e1dfa8e9dd774c722838
bykoon <bykoon@bemily.com>
2022-06-07 22:08:23 +0900

    2022 kakao blind recruitment md 추가

---
+++ algorithm/프로그래머스/KAKAO/신고결과받기/java/main/solution.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+    }
+
+
+}
---
+++ algorithm/프로그래머스/KAKAO/신고결과받기/java/test/TestSolution.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class TestSolution {
+    @Test
+    public int[] solution(String[] id_list, String[] report, int k) {
+    }
+    
+    
+}
