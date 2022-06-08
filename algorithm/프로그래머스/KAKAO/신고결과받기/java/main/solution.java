9cce868452d402a40d88492b018a90ad27b9a1aa
bykoon <bykoon@bemily.com>
2022-06-08 09:06:43 +0900

    카카오 코테 문제1 추가

--- algorithm/프로그래머스/KAKAO/신고결과받기/java/main/solution.java
+++ algorithm/프로그래머스/KAKAO/신고결과받기/java/main/solution.java
@@ -1,5 +1,7 @@
 public class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        return answer;
     }
 
 
--- algorithm/프로그래머스/KAKAO/신고결과받기/java/test/TestSolution.java
+++ algorithm/프로그래머스/KAKAO/신고결과받기/java/test/TestSolution.java
@@ -3,6 +3,8 @@ import org.junit.Test;
 public class TestSolution {
     @Test
     public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        return answer;
     }
     
     
