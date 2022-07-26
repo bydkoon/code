5fabdf68265663ca0740e573596fe8d248c8ef09
bykoon <bykoon@bemily.com>
2022-07-26 09:08:00 +0900

    Template 클래스명수정

--- algorithm/template/java/main/template.java
+++ algorithm/template/java/main/template.java
@@ -1,6 +1,13 @@
-public class template {
+public class Template {
     public void solution(int[] A){
+
     }
-    
-    
+
+    public static void main(String[] args) {
+        int K = 4;
+        int[] A = {1,2,3,4,1,1,3};
+        Template template = new Template();
+        int count = template.solution(K, A);
+        System.out.println(count);
+    }
 }
