191fa2e24c5e9fbb9af4112f6a0809a563a331ff
Dohoon Kim <pansew@gmail.com>
2022-08-05 00:25:16 +0900

    피보나치 함수 추가

---
+++ algorithm/DP/fibo.py
@@ -0,0 +1,23 @@
+# def fibo(x):
+#     if x == 1 or x ==2:
+#         return 1
+#     return fibo(x-1)+fibo(x-2)
+
+# print(fibo(4))
+
+#100개의 배열을 미리 할당을 받아둔다
+d = [0] * 100
+
+def fibo(x):
+    #초기값이 1,2인경우 결과를 1로 반환한다.
+    if x == 1 or x == 2:
+        return 1
+
+    #메모리 데이터에 0이아닌경우는 배열에서 이미 계산된 값을 반환한다ㅏ.
+    if d[x] != 0:
+        return d[x]
+    #d메모리에 없을경우는 fibo 재귀호출 한다.    
+    d[x] = fibo(x-1) + fibo(x-2)
+    return d[x]
+
+print(fibo(99))
