6fab8c8972c19ee3a926c89b13a97ede09e04d4d
Dohoon Kim <pansew@gmail.com>
2022-08-08 23:45:19 +0900

    코드 수정

--- algorithm/DP/fibo6.py
+++ algorithm/DP/fibo6.py
@@ -1,5 +1,9 @@
 
 #테이스 케이스 (Test Case)입력
+#1
+# 3 4
+#1 3 3 2 2 1 4 1 0 6 4 7
+
 for tc in range(int(input())):
     #금광 정보 입력
     n,m = map(int, input().split())
@@ -16,17 +20,20 @@ for tc in range(int(input())):
     for j in range(1, m):
         for i in range(n):
             #왼쪽 위에서 오는 경우 
-            if i ==0: left_up =0
-            else: left_up = dp[i-1][j-1]
+            if i == 0: 
+                left_up = 0
+            else: 
+                left_up = dp[i-1][j-1]
             #왼쪽 아래에서 오는경우
-            if i ==n -1: left_down = 0
-            else: left_down = dp[i+1][j-1]
+            if i == n -1: 
+                left_down = 0
+            else: 
+                left_down = dp[i+1][j-1]
             #왼쪽에서 오는 경우
             left = dp[i][j-1]
             dp[i][j] = dp[i][j] + max(left_up, left_down, left)
+
     result = 0
     for i in range(n):
         result = max(result, dp[i][m-1])
     print(result)
-
-    
